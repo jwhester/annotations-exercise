01e322732bc97cd31033b538fbd3dbf648e6fd61
jwhester <jameshester88@gmail.com>
2019-12-06 12:03:40 -0600

    fixed errors

--- src/Author.java
+++ src/Author.java
@@ -5,17 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
-    /**
-     * @deprecated Use publishedBooks instead
-     */
-    @Deprecated
+
+
     public List<String> getBooks() {
         return books;
     }
@@ -28,7 +26,6 @@ public class Author extends Person {
         books.add(book);
     }
 
-    @Override
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
